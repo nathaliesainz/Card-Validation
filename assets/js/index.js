f062c27469654d428e3802ad027d063df78c3b0c
nathaliesainz <nathaliesainz13@gmail.com>
2022-01-23 15:49:25 -0600

    Initial Commit

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+function inicio(){
+    document.getElementById("content").style.display="none"
+    document.getElementById("firstBlock").style.display="block"
+}
+
+function validar(){
+    
+} 
+
+var cardNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
