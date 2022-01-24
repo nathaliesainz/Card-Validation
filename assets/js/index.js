e9105b98c4a76e36b5536a26d98017255e8cf0e0
nathaliesainz <nathaliesainz13@gmail.com>
2022-01-23 20:04:16 -0600

    luhn algorithm v1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,39 @@
-function inicio(){
-    document.getElementById("content").style.display="none"
-    document.getElementById("firstBlock").style.display="block"
-}
+const stringCreditNumber = '4137894711755904'
+const cardNumber = stringCreditNumber.split('');
+const doubleDigits = [];
+let totalAdd = 0;
 
-function validar(){
-    
-} 
+for (let cardDigit = 0; cardDigit < cardNumber.length; cardDigit += 2) {
+    const pairsDigits = cardNumber[cardDigit];
+    doubleDigits.push(pairsDigits * 2);
+};
 
-var cardNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const greaterNine = doubleDigits.map((digit) => {
+    if (digit > 9) {
+        return ((digit - 1) % 9) + 1;
+    }
+});
+
+const filterUndefined = greaterNine.filter((value) => {
+    return value !== undefined;
+});
+
+const filterLessNine = doubleDigits.filter((value) => {
+    return value <= 9;
+});
+
+const arrayResult = filterLessNine.concat(filterUndefined);
+const oddFilter = cardNumber.filter((pair, odd) => odd % 2 === 1);
+const finalArray = arrayResult.concat(oddFilter);
+
+const toNumber = finalArray.map((element) => {
+    return parseInt(element)
+});
+
+toNumber.forEach((value) => {
+    totalAdd += value;
+});
+
+totalAdd % 10 === 0 
+    ? console.log(`${totalAdd} :¨Valid credit card`)
+    : console.log(`${totalAdd} :¨Invalid credit card`);
